6b8b01c6118522618fcfa91815b9f98cc4d5b86a
wangtonghe <ansonwang@foxmail.com>
2022-10-08 02:27:46 +0800

    添加远程武器配置

--- Assets/Scripts/UI/GameScene/Inventory/UI_ItemSlot.cs
+++ Assets/Scripts/UI/GameScene/Inventory/UI_ItemSlot.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fa55c55bef01572914060bf2a67a36b2f3c83dd73848d14e8cd557c6478e621e
-size 9306
+oid sha256:75571c232d552305ecc10c1ad6a7089650a692440a2d9ae394d5252f9edbf4d9
+size 9600
